0becbf33bff6dfdc7775be1a08fdaebb5b1e9633
Evaldas K <efka636lt@gmail.com>
2020-05-19 20:09:39 +0300

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ connection.once("open", ()=> {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './')));
+  app.use(express.static(path.join(__dirname, '.')));
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
